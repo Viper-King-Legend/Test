bd375049d881a8c4f711b4792865df177642dcce
Viper-King-Legend <78768501+Viper-King-Legend@users.noreply.github.com>
2021-07-15 10:40:45 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ block_image_width = 5;
 block_image_height = 5;
 
 function load_img(){
-	fabric.Image.fromURL("golf-h1.png", function(Img) {
+	fabric.Image.fromURL("golf-h.png", function(Img) {
 		hole_obj = Img;
 		hole_obj.scaleToWidth(50);
 		hole_obj.scaleToHeight(50);
@@ -20,7 +20,6 @@ function load_img(){
 		});
 		canvas.add(hole_obj);
 	});
-	new_image();
 }
 
 function new_image()
@@ -78,7 +77,7 @@ function my_keydown(e)
 		if(ball_y >=5){
 			ball_y = ball_y - block_image_height;
 			console.log("block image height = " - block_image_height);
-			console.log("When Up arrow key is pressed, X = " - ball_x - " , Y = "-ball_y);
+			console.log("When Up arrow key is pressed, X = " - ball_x - " , Y = "+ball_y);
 			canvas.remove(ball_obj);
 			new_image();
 		}
@@ -89,7 +88,7 @@ function my_keydown(e)
 		 if(ball_y <=450){
 			 ball_y = ball_y + block_image_height;
 			 console.log("block image height = " + block_image_height);
-			 console.log("When Down arrow key is pressed, X = " + ball_x + " , y = "=ball_y);
+			 console.log("When Down arrow key is pressed, X = " + ball_x + " , y = "+ball_y);
 			 canvas.remove(ball_obj);
 			 new_image();
 		 }
@@ -101,7 +100,7 @@ function my_keydown(e)
 		{
 			ball_x = ball_x - block_image_width;
 			console.log("block image width = " - block_image_width);
-			console.log("When Left arrow key is pressed, Y = " - ball_y - " , X = "-ball_x);
+			console.log("When Left arrow key is pressed, Y = " - ball_y - " , X = "+ball_x);
 			canvas.remove(ball_obj);
 			new_image();
 		}
